34687c200ff267114f1d904a933147862ebf4fbc
Ari Asulin <ari.asulin@gmail.com>
2020-07-21 14:29:12 -0700

    sample torrent. remove wav files

--- src/samples/snes/snes.library.js
+++ src/samples/snes/snes.library.js
@@ -1,13 +1,13 @@
 // import FileService from "../../song/file/FileService";
 
 const SNESLibraryIndex = {
-	magnet: "magnet:?xt=urn:btih:76cae1312f250071f644631b6cffd3fb1645e50b&dn=fws&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com",
-	baseURL: "torrent://76cae1312f250071f644631b6cffd3fb1645e50b/",
+	magnet: "magnet:?xt=urn:btih:a31531cc4c064024f2e00ad8d869d8aa2129c3c6&dn=snesology-samples&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&tr=wss%3A%2F%2Ftracker.webtorrent.io",
+	baseURL: "torrent://a31531cc4c064024f2e00ad8d869d8aa2129c3c6/",
 	// libraryFile: "library.json",
 	title: 'SNES Libraries',
 	libraries: () => {
 		return [
-			new SNESLibrary(require('./sets/ffvi.library.json'))
+			new SNESLibrary(require('./ffvi/ffvi.library.json'))
 		]
 	}
 }
@@ -18,39 +18,33 @@ export default SNESLibraryIndex;
 class SNESLibrary {
 	constructor(libraryData) {
 		this.data = libraryData;
-		console.log('SNESLibrary', this);
+		console.log('SNESLibrary', libraryData);
 	}
 
 	get title() { return this.data.title; }
 
 	presets(programClassFilter) {
+		const presets = this.data.presets.map(presetData => {
+			const presetConfig = Object.assign({}, presetData);
+			if(presetData.tags)
+				presetConfig.tags = presetData.tags;
+			presetConfig.voices = presetData.voices.map(sampleData => ['AudioBufferInstrument', Object.assign({}, sampleData, {
+				url: SNESLibraryIndex.baseURL + this.data.urlPrefix + sampleData.url,
+			})])
+			return ['PolyphonyInstrument', presetConfig];
+		});
 
-		// console.log('programClassFilter', programClassFilter);
+		console.log('presets', presets);
 		switch(programClassFilter) {
 			default:
 				case 'PolyphonyInstrument':
-			return this.data.presets.map(presetData => {
-				const presetConfig = {
-					title: presetData.title,
-					voices: []
-				}
-				if(presetData.tags)
-					presetConfig.tags = presetData.tags;
-				presetConfig.voices = presetData.voices.map(sampleData => ['AudioBufferInstrument', Object.assign({}, sampleData, {
-					url: SNESLibraryIndex.baseURL + this.data.sampleFolder + sampleData.url,
-				})])
-				// if(LibraryData.baseURL)
-				// 	presetData.url = LibraryData.baseURL + presetData.url;
-				return ['PolyphonyInstrument', presetConfig];
-			});
+			return presets
 
 		case 'AudioBufferInstrument':
 			const voices = [];
-			this.data.presets.forEach(presetData => {
-				presetData.voices.forEach(sampleData => {
-					voices.push(['AudioBufferInstrument', Object.assign({}, sampleData, {
-						url: SNESLibraryIndex.baseURL + this.data.sampleFolder + sampleData.url,
-					})]);
+			presets.forEach(presetData => {
+				presetData.voices.forEach(voicePreset => {
+					voices.push(voicePreset);
 				})
 			});
 			return voices;
