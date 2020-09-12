45b3d5e4c5cb180793b7d124602cc210c0b82d55
Ari Asulin <ari.asulin@gmail.com>
2020-09-12 13:04:01 -0700

    song publishing

--- scripts/startServer.js
+++ scripts/startServer.js
@@ -2,7 +2,7 @@ import Server from "../src/audio-source/server/Server";
 
 const path = require('path');
 const serverConfig = require('../src/audio-source/server/.server.json');
-serverConfig.publicDirectory = path.resolve(__dirname, '../public');
+serverConfig.publicDirectory = path.resolve(__dirname, '../private/public');
 serverConfig.privateDirectory = path.resolve(__dirname, '../private');
 // console.log('serverConfig', serverConfig)
 const server = new Server();
